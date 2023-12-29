0b1c08b8397777eaa84b7625176285d7ea07cb3b
Bharat Bhusal <bharatbhusal78@gmail.com>
2023-12-29 21:11:06 +0530

    type conversion

---
+++ src/Converter.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract StringToBytes32 {
+    mapping(bytes32 => string) private reverseMapping;
+
+    function stringToBytes32(string memory s) public returns (bytes32) {
+        bytes32 hash = keccak256(abi.encodePacked(s));
+        reverseMapping[hash] = s;
+        return hash;
+    }
+
+    function bytes32ToString(bytes32 hash) public view returns (string memory) {
+        return reverseMapping[hash];
+    }
+}
--- test/CTF1Test.sol
+++ test/CTF1Test.sol
@@ -2,15 +2,9 @@
 pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
-import {CTF1} from "../src/CTF1.sol";
 
 contract CTF1Test is Test {
-    CTF1 public ctf1;
-    bytes32 private password;
-
-    function setUp() public {
-        ctf1 = new CTF1(password);
-    }
+    function setUp() public {}
 
     function test_unlock() public {}
 }
