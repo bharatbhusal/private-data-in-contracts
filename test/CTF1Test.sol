7c40b83715b8308b58a62b49c58d82a5e626bbfa
Bharat Bhusal <bharatbhusal78@gmail.com>
2023-12-29 20:43:43 +0530

    found the passwrod

--- test/CTF1Test.sol
+++ test/CTF1Test.sol
@@ -4,12 +4,12 @@ pragma solidity ^0.8.13;
 import {Test, console2} from "forge-std/Test.sol";
 import {CTF1} from "../src/CTF1.sol";
 
-contract CounterTest is Test {
-    CTF1 public counter;
+contract CTF1Test is Test {
+    CTF1 public ctf1;
+    bytes32 private password;
 
     function setUp() public {
-        bytes32 password;
-        counter = new CTF1(password);
+        ctf1 = new CTF1(password);
     }
 
     function test_unlock() public {}
