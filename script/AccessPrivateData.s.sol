81e9fd50e5c4b78a7c8ca734656a03a381c6d99e
Bharat Bhusal <bharatbhusal78@gmail.com>
2024-01-03 20:09:38 +0530

    corrected

--- script/AccessPrivateData.s.sol
+++ script/AccessPrivateData.s.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.13;
 import "forge-std/Script.sol";
 import "forge-std/console2.sol";
 import "../src/CTF1.sol";
-import "../src/StringBytes32.sol";
-import "../src/StringAddress.sol";
+
+// import "../src/StringBytes32.sol";
+// import "../src/StringAddress.sol";
 
 contract AccessPrivateData is Script {
     function getPrivateData(
@@ -21,19 +22,15 @@ contract AccessPrivateData is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        StringBytes32 stringBytes32Converter = new StringBytes32();
-        StringAddress stringAddressConverter = new StringAddress();
+        // StringBytes32 stringBytes32Converter = new StringBytes32();
+        // StringAddress stringAddressConverter = new StringAddress();
 
-        uint256 privateStateSlot = 0;
-        address contractAddress = stringAddressConverter.stringToAddress(
-            "0x89d5b48f3974A05b4BF816aebA12D401c0ebb003"
-        );
+        uint256 privateStateSlot = 1;
+        address contractAddress = 0x89d5b48f3974A05b4BF816aebA12D401c0ebb003;
 
-        string memory privateData = stringBytes32Converter.bytes32ToString(
-            getPrivateData(privateStateSlot, contractAddress)
-        );
+        bytes32 privateData = getPrivateData(privateStateSlot, contractAddress);
 
-        console2.log(privateData);
+        console2.logBytes32(privateData);
         console2.log("hi");
         vm.stopBroadcast();
     }
